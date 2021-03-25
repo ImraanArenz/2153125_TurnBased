d0f1ef3afc22095b6f10ac1c0c02af7f49e01b18
ImraanArenz <2153125@students.wits.ac.za>
2021-03-26 00:59:45 +0200

    Commit_001

---
+++ 2153125 - Turn Based/Assets/Scripts/GameUnitScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUnitScript : MonoBehaviour
+{
+    public string EnemyUnitName;
+    public int EnemyUnitLvl;
+    public int EnemyDamageOutput;
+    public int MaximumHealth;
+    public int CurrentHealth;
+}
---
+++ 2153125 - Turn Based/Assets/Scripts/TurnBasedSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleStateMachine { BeginBattle, PlayerTurn, EnemyTurn, PlayerWin, PlayerLose}
+public class TurnBasedSystem : MonoBehaviour
+{
+    public BattleStateMachine CurrentState;
+    public Transform PlayerPos;
+    public Transform enemyPos;
+
+    private void Start()
+    {
+        CurrentState = BattleStateMachine.BeginBattle;
+        StartBattle();
+    }
+
+    void StartBattle()
+    {
+        Instantiate(playerPrefab)
+
+    }
+}
